f18d1f7cb777a7a5d1bc3d5e4368c93c31cccc5e
Hsiaoming Yang <me@lepture.com>
2025-01-10 22:23:21 +0900

    feat: attach readme when submitting the theme

--- src/release.mjs
+++ src/release.mjs
@@ -1,15 +1,28 @@
 import fetch from "node-fetch"
+import { readdir, readFile } from "fs/promises"
 
 const API_URL = process.env.THEME_SUBMIT_API || 'https://api.typlog.com/v4/theme/submit'
 
 export async function submitTheme (theme, token) {
+  const readme = await findReadme()
+  const body = JSON.stringify({...theme, readme})
   const resp = await fetch(API_URL, {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${token}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(theme),
+    body,
   })
   return resp.json()
+}
+
+async function findReadme () {
+  const names = await readdir(process.cwd())
+  for (let name of names) {
+    if (/readme\./i.test(name)) {
+      return readFile(name, {encoding: 'utf-8'})
+    }
+  }
+  return ''
 }
